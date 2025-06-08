d069900a80e293905865612f0b672a509f7cfa4d
dungitonga <164783850+dungitonga@users.noreply.github.com>
2025-06-08 22:06:38 +0300

    Update settings.py

--- payment/settings.py
+++ payment/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-c_9dnj(_syp&)-5!g9fw$thc1y5!#z+5w^0#t0hetq75lx(k*^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [.onrender.com]
 
 
 # Application definition
