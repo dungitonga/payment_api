a4baec9991014034aa3dacaf759003bc36e4d087
dungitonga <164783850+dungitonga@users.noreply.github.com>
2025-06-09 20:53:40 +0300

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+python -m pip install --upgrade pip
+
+python -m pip install -r requirements.txt
+python -m pip install --force-reinstall -U setuptools
+
+python manage.py collectstatic --no-input
+python manage.py makemigrations
+python manage.py migrate
